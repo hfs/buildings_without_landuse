eddff26ff9fccd1ced18037f5054abd3bb3038a4
Hermann Schwarting <hfs@gmx.de>
2021-03-20 16:54:28 +0100

    Allow to overrides states on the command line

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-export STATES="baden-wuerttemberg bayern brandenburg bremen hamburg hessen mecklenburg-vorpommern niedersachsen nordrhein-westfalen rheinland-pfalz saarland sachsen sachsen-anhalt schleswig-holstein thueringen"
+export STATES="${STATES:-baden-wuerttemberg bayern brandenburg bremen hamburg hessen mecklenburg-vorpommern niedersachsen nordrhein-westfalen rheinland-pfalz saarland sachsen sachsen-anhalt schleswig-holstein thueringen}"
 export PGHOST=localhost
 export PGPORT=5432
 export PGUSER=postgres
